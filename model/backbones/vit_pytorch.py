5fb24a4e0becf569fd39c208428b7d685d70e396
Daniel García <50992677+danigarci1@users.noreply.github.com>
2023-08-29 17:54:28 +0200

    Replaces container_abcs with collections.abc

--- model/backbones/vit_pytorch.py
+++ model/backbones/vit_pytorch.py
@@ -27,13 +27,12 @@ from itertools import repeat
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch._six import container_abcs
-
+import collections.abc
 
 # From PyTorch internals
 def _ntuple(n):
     def parse(x):
-        if isinstance(x, container_abcs.Iterable):
+        if isinstance(x, collections.abc.Iterable):
             return x
         return tuple(repeat(x, n))
     return parse
